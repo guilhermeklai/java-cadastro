f1b408c5ae96c8450cf83fe4b8c5d46734a9a95a
Guilherme Klai <gklaidossantos@gmail.com>
2019-02-21 16:36:12 -0300

    Exercicio de JS cadastro

---
+++ script.js
@@ -0,0 +1,34 @@
+// let name = prompt('Qual seu nome?')
+// console.log('Seu nome é: ' + name)
+
+// let username = prompt('Escreva o seu username:')
+// console.log('Seu username é: ' + username)
+
+// let password = prompt('Escolha uma senha:')
+
+// let passwordcorfirm = prompt('Confirme sua senha:')
+//     if (password != passwordcorfirm){
+//         alert('As senhas não coincidem, escreva-as denovo.')
+//         password = prompt('Escreva a sua senha:')
+//         passwordcorfirm = prompt('Confirme sua senha:')
+//     }
+// console.log('A sua senha é: ' + passwordcorfirm)
+
+// let day = Number(prompt('Em qual dia que você nasceu? ( 1 até 31)'));
+//     while (day < 1 || day > 31 || day == NaN){
+//         day = Number(prompt('Data invalida, coloque o dia do seu nascimento denovo:'));
+//     }
+
+// let month = Number(prompt('Em qual mês você nasceu? (1 - 12)'));
+//     while (month < 1 || month > 12 || month == NaN){
+//         month = Number(prompt('Data invalida, coloque o mês do seu nascimento denovo:'))
+// }
+
+// let year = Number(prompt('Em qual ano você nasceu? (1998, 1920 e etc'));
+//     while (year < 1900 || year > 2019 || year == NaN){
+//         year = Number(prompt('Você ainda não existe ou já não existe mais, coloque o ano do seu nascimento denovo:'))
+// }
+
+// let nascimento = ('Data de nascimento: ' + day + '/' + month + '/' + year)
+// console.log(nascimento)
+
